823b40dd29a7efbfa553d9ca20a8445608c85fad
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-07-08 22:41:22 -0300

    criando a base do projeto e as primeiras declarações com reactive e diretivas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// fizemos a instalação do bootstrap via terminal e com o import acima
+// eu importo um arquivo específico dessa biblioteca de estilo presente
+// agora dentro de node_modules
 
 createApp(App).mount('#app')
